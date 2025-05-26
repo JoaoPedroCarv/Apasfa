b505f664b9f68e958ba1e78c6c043a72b6916976
izaque674 <izaquefs88@hotmail.com>
2025-05-26 08:25:23 -0300

    ta quase

--- src/pages/admin/editar/editar.js
+++ src/pages/admin/editar/editar.js
@@ -6,7 +6,7 @@ import '../admin.css';
 const Editar = () => {
     return (
         <div className="admin-dashboard-container" style={{ justifyContent: 'center' }}>
-            <main className="admin-main-content" style={{ maxWidth: 1100, width: '100%' }}>
+            <main className="admin-main-content" style={{ maxWidth: 1300, width: '100%' }}>
                 <h2 style={{
                     color: 'var(--primary)',
                     fontWeight: 700,
@@ -18,17 +18,19 @@ const Editar = () => {
                     Editar Informações
                 </h2>
 
+                {/* Eventos */}
                 <div className="admin-cards-wrapper">
                     <section className="admin-section-card">
                         <h3>Eventos</h3>
                         <EventosAdmin />
                     </section>
-
-                    <section className="admin-section-card">
-                        <h3>Animais</h3>
-                        <AnimaisAdmin />
-                    </section>
                 </div>
+
+                
+                <section className="admin-section-card" style={{ maxWidth: '100%' }}>
+                    <h3>Animais</h3>
+                    <AnimaisAdmin />
+                </section>
             </main>
         </div>
     );
